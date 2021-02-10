70869b2197c3ec0832b0b90e7ac46b9484091f60
Michael Kistler <michaelkis213@yahoo.com>
2021-02-10 14:03:50 -0500

    Add functionality to search Wikipedia
    Add function (get_wiki_page) that takes a keyword as input and
    downlaods the HTML content of that Wikipedia page.
    
    Add child class (ParagraphParser) of HTMLParser that is able get the
    contents within all <p> elements in the HTML.
    
    Add function (find_keywords) that uses ParagraphParser to search
    HTML for a pragraph containing the passed in keywords.

---
+++ content-generator.py
@@ -0,0 +1,70 @@
+import tkinter
+import sys
+import csv
+from urllib.request import urlopen
+from html.parser import HTMLParser
+
+
+class ParagraphParser(HTMLParser):
+    '''
+    Custom HTMLParser class used for parsing data 
+    inside of <p> tags from Wikipedia pages.
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.in_paragraph = False
+        self.paragraphs = ['']
+        self.index = 0
+
+    def handle_starttag(self, tag, attrs):
+        '''Set in_paragraph when a <p> tag is found.'''
+        if tag == 'p':
+            self.in_paragraph = True
+
+    def handle_data(self, data):
+        '''Read all data found inside <p> tags.'''
+        if self.in_paragraph:
+            self.paragraphs[self.index] += data
+
+    def handle_endtag(self, tag):
+        '''Handle when a closing </p> tag is found.'''
+        if tag == 'p':
+            self.in_paragraph = False
+            self.index += 1
+            self.paragraphs.append('')
+
+
+def get_wiki_page(keyword):
+    '''Downlaods the contents of the specified keywords Wikipedia page.'''
+    # Open the Wiki page of the keyword
+    url = f'https://en.wikipedia.org/wiki/{keyword}'
+    wiki = urlopen(url)
+    wiki_contents = wiki.read().decode('utf-8')
+    return wiki_contents
+
+
+def find_keywords(wiki_contents, primary, secondary):
+    '''Finds the pargraph containing the primary and secondary keywords.'''
+    # Parse the content for all data inside <p> elements
+    parser = ParagraphParser()
+    parser.feed(wiki_contents)
+
+    # Find paragraph that contains both keywords
+    for paragraph in parser.paragraphs:
+        if (
+            primary.lower() in paragraph.lower()
+            and secondary.lower() in paragraph.lower()
+        ):
+            return paragraph.strip()
+
+    # No paragraph found with both words
+    return f'No paragraph found containing {primary} and {secondary}.'
+
+
+if __name__ == '__main__':
+    primary = 'Bottle'
+    secondary = 'carbonated'
+    wiki_contents = get_wiki_page(primary)
+    paragraph = find_keywords(wiki_contents, primary, secondary)
+    print(paragraph)
