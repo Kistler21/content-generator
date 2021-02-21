d2f80a8352c1aad12302d990090878a011670796
Michael Kistler <michaelkis213@yahoo.com>
2021-02-20 20:30:59 -0500

    Sprint3 (#1)
    * Add functionality to search Wikipedia
    
    Add function (get_wiki_page) that takes a keyword as input and
    downlaods the HTML content of that Wikipedia page.
    
    Add child class (ParagraphParser) of HTMLParser that is able get the
    contents within all <p> elements in the HTML.
    
    Add function (find_keywords) that uses ParagraphParser to search
    HTML for a pragraph containing the passed in keywords.
    
    * Update program to allow CSV input
    
    The program will now be able to get keywords from  a CSV file.
    The file must be passed as an argument in the command line when
    starting the program. If no CSV file is passed, the program uses
    hard-coded keywords that must be changed within the code file.
    
    * Update program to write output to CSV file
    
    The program now writes the output result of searching Wikipedia to a
    CSV file named 'output.csv'. The program will write over any
    existing data in the file if it already exists. The CSV file has
    headers input_keywords and output_content.
    
    * Create a GUI with no functionality
    
    This commit adds a GUI where a user can enter a primary and secondary
    keywords. The button to generate output currently does not do
    anything. The functionality will be added later.
    
    * Implement all functionality of GUI
    
    The button to generate output now generates correct output based
    on the inputted primary and secondary keywords.
    
    * Add responsiveness to GUI elements
    
    The GUI elements now resize dynamically when the window is resized.
    The program feels much more responsive now.
    
    * Add error handling
    
    The program will now output an error message to the text box if the
    either keyword is invalid. The program also displays a message letting
    the user know if no paragraph was found containing both keywords.
    
    * Handle bad requests to Wikipedia
    
    The program will now display an error message in the output text box
    if Wikipedia has no webpage for the primary keyword.

---
+++ content-generator.py
@@ -0,0 +1,245 @@
+import tkinter as tk
+import sys
+import csv
+from urllib.request import urlopen, HTTPError
+from html.parser import HTMLParser
+
+
+class ParagraphParser(HTMLParser):
+    '''
+    Custom HTMLParser class used for parsing data
+    inside of <p> tags from Wikipedia pages.
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.in_paragraph = False
+        self.paragraphs = ['']
+        self.index = 0
+
+    def handle_starttag(self, tag, attrs):
+        '''Set in_paragraph when a <p> tag is found.'''
+        if tag == 'p':
+            self.in_paragraph = True
+
+    def handle_data(self, data):
+        '''Read all data found inside <p> tags.'''
+        if self.in_paragraph:
+            self.paragraphs[self.index] += data
+
+    def handle_endtag(self, tag):
+        '''Handle when a closing </p> tag is found.'''
+        if tag == 'p':
+            self.in_paragraph = False
+            self.index += 1
+            self.paragraphs.append('')
+
+
+def get_wiki_page(keyword):
+    '''
+    Downloads the contents of the specified keywords Wikipedia page.
+    Return false if the page doesn't exist.
+    '''
+    # Open the Wiki page of the keyword
+    try:
+        url = f'https://en.wikipedia.org/wiki/{keyword}'
+        wiki = urlopen(url)
+        wiki_contents = wiki.read().decode('utf-8')
+        return wiki_contents
+    except HTTPError:
+        return False
+
+
+def find_keywords(wiki_contents, primary, secondary):
+    '''
+    Finds the pargraph containing the primary and secondary keywords.
+    Returns False if no pargraph is found containing both keywords.
+    '''
+    # Parse the content for all data inside <p> elements
+    parser = ParagraphParser()
+    parser.feed(wiki_contents)
+
+    # Find paragraph that contains both keywords
+    for paragraph in parser.paragraphs:
+        if (
+            primary.lower() in paragraph.lower()
+            and secondary.lower() in paragraph.lower()
+        ):
+            return paragraph.strip()
+
+    # No paragraph found with both words
+    return False
+
+
+def read_csv(file_name):
+    '''Reads keywords from a CSV file.'''
+    # Open the file
+    with open(file_name, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+
+        # Read the contents
+        for keywords in csv_reader:
+            return keywords['input_keywords'].split(';')
+
+
+def csv_output(primary, secondary, paragraph):
+    '''Writes the output to a CSV file named output.csv.'''
+    # Open the file
+    with open('output.csv', 'w', newline='') as csv_file:
+        headers = ['input_keywords', 'output_content']
+        csv_writer = csv.DictWriter(csv_file, fieldnames=headers)
+
+        # Write contents to file
+        output = {
+            headers[0]: f'{primary};{secondary}',
+            headers[1]: paragraph
+        }
+        csv_writer.writeheader()
+        csv_writer.writerow(output)
+
+
+def main():
+    '''Contains all the functionality of the GUI.'''
+
+    def generate():
+        '''Function that is called when the generate button is pressed.'''
+        # Enable the textbox and delete the old contents
+        output_txt['state'] = 'normal'
+        output_txt.delete('1.0', tk.END)
+
+        # Get the primary keywords from the entry boxes
+        primary = primary_ent.get()
+        secondary = secondary_ent.get()
+
+        # Check the format of the keywords
+        if not primary.isalpha() or not secondary.isalpha():
+            # Place error message
+            message = 'Primary and secondary keywords can only contain uppercase or lowercase letters and cannot be empty.'
+            output_txt['fg'] = 'red'
+            output_txt.insert('1.0', message)
+            return
+
+        # Download and validate the webpage
+        wiki_contents = get_wiki_page(primary)
+        if not wiki_contents:
+            message = 'No Wikipedia page for specified primary keyword.'
+            output_txt['fg'] = 'red'
+            output_txt.insert('1.0', message)
+            return
+
+        # Parse the HTML for a valid paragraph
+        paragraph = find_keywords(wiki_contents, primary, secondary)
+
+        # Check if a valid paragraph was found
+        if not paragraph:
+            # Place error message
+            message = f'No paragraph was found containing {primary} and {secondary}.'
+            output_txt['fg'] = 'red'
+            output_txt.insert('1.0', message)
+            return
+
+        # Place the output in the textbox
+        output_txt['fg'] = 'black'
+        output_txt.insert('1.0', paragraph)
+        output_txt['state'] = 'disabled'
+        csv_output(primary, secondary, paragraph)
+
+    # Open the main menu for the GUI
+    window = tk.Tk()
+    window.title('Content Generator')
+
+    # Configure rows and columns of window
+    window.rowconfigure(3, weight=1)
+    window.columnconfigure(1, weight=1)
+
+    # Create the label and entry for primary keyword
+    primary_lbl = tk.Label(window, text='Primary Keyword:')
+    primary_ent = tk.Entry(window)
+
+    # Place the primary keyword label and entry
+    primary_lbl.grid(
+        row=0,
+        column=0,
+        sticky='e',
+        pady=10,
+        padx=(5, 0)
+    )
+    primary_ent.grid(
+        row=0,
+        column=1,
+        padx=(0, 5),
+        sticky='ew'
+    )
+
+    # Create the label and entry for secondary keyword
+    secondary_lbl = tk.Label(window, text='Secondary Keyword:')
+    secondary_ent = tk.Entry(window)
+
+    # Place the secondary keyword label and entry
+    secondary_lbl.grid(
+        row=1,
+        column=0,
+        sticky='e',
+        padx=(5, 0)
+    )
+    secondary_ent.grid(
+        row=1,
+        column=1,
+        sticky='ew',
+        padx=(0, 5)
+    )
+
+    # Create and place the button for generating the output
+    generate_btn = tk.Button(
+        window,
+        text='Generate',
+        bg='lightgray',
+        command=generate
+    )
+    generate_btn.grid(
+        row=2,
+        column=0,
+        pady=20,
+        ipady=10,
+        ipadx=10,
+        columnspan=2
+    )
+
+    # Create the label and text for output
+    output_lbl = tk.Label(window, text='Generated Output:')
+    output_txt = tk.Text(
+        window,
+        height=15,
+        width=50,
+        wrap=tk.WORD,
+        state='disabled'
+    )
+
+    # Place the output label and text
+    output_lbl.grid(
+        row=3,
+        column=0,
+        sticky='ne',
+        padx=(5, 0),
+    )
+    output_txt.grid(
+        row=3,
+        column=1,
+        sticky='nsew',
+        padx=(0, 5),
+        pady=(0, 10)
+    )
+
+    # Check if csv is passed on command line
+    if len(sys.argv) == 2:
+        primary, secondary = read_csv(sys.argv[1])
+        primary_ent.insert(0, primary)
+        secondary_ent.insert(0, secondary)
+        generate()
+
+    # Run the loop to look for events
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
